061d7cd19dd215c519ff4f9fc2f69b2d55b7755d
s3bull <95282477+s3bull@users.noreply.github.com>
2022-02-15 15:12:06 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const inputText = document.querySelector('#name-input');
 const outputText = document.querySelector('#name-output');
 inputText.addEventListener('input', (event) => {
     const name = event.currentTarget.value;
-    inputText.value === '' ? outputText.textContent = 'Anonymous' : outputText.textContent = name;
+    outputText.textContent = inputText.value === '' ? 'Anonymous' :  name;
 });
